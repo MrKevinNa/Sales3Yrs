d96bb9355c7c2a14955b5aec52b54015675aa5bc
MrKevinNa <drkevin2022@gmail.com>
2020-11-28 22:34:56 +0900

    edit R function

--- R/draw_barplot.R
+++ R/draw_barplot.R
@@ -12,6 +12,7 @@
 #' }
 
 draw_barplot <- function(data) {
+
   ui <- fluidPage(
     sidebarPanel(
       textInput(inputId = 'compName',
@@ -20,23 +21,28 @@ draw_barplot <- function(data) {
       submitButton(text = 'Submit',
                    icon = icon(name = 'submit'))
     ),
-    mainPanel(
-      plotOutput(outputId = 'barplot', width = '100%')
-    )
+    mainPanel(uiOutput(outputId = 'mainUI'))
   )
 
   server <- function(input, output, session) {
     output$barplot <- renderPlot({
-      # df <- data %>% filter(Company == input$compName)
       df <- data[data$Company == input$compName, ]
       ggplot(mapping = aes(x = df$Year, y = df$Sales)) +
         geom_col(fill = 'gray80') +
         scale_y_continuous(expand = expansion(mult = 0.1)) +
         geom_text(mapping = aes(label = df$Sales, vjust = -1)) +
-        ggtitle(label = str_glue('Sales Volume of {input$compName}')) +
+        ggtitle(label = str_glue('Sales volume of {input$compName}')) +
         theme_bw() +
         theme(text = element_text(family = 'NanumGothic'),
               plot.title = element_text(hjust = 0.5, face = 'bold'))
+    })
+
+    output$mainUI <- renderUI({
+      if(input$compName == '') {
+        h4('There is no plot before typing in Company Name!')
+      } else {
+        plotOutput(outputId = 'barplot', width = '100%')
+      }
     })
   }
 
