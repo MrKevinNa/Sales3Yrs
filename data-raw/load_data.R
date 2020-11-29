1fd434f2f87bb2cf8324a19e48c4e51e57da8ff9
MrKevinNa <drkevin2022@gmail.com>
2020-11-29 13:36:50 +0900

    add csv file

--- data-raw/load_data.R
+++ data-raw/load_data.R
@@ -12,5 +12,8 @@ sales <- readRDS(file = './data-raw/results.RDS')
 # tibble로 변환
 sales <- as_tibble(x = sales)
 
+# csv 파일로 저장
+write.csv(x = sales, file = './data-raw/sales.csv', row.names = FALSE)
+
 
 ## End of Document
